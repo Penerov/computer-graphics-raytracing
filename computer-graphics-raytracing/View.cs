937de861b9a3ca9300a97da268fbeffbf7a081f2
anton <anton@DESKTOP-6FI73JL>
2019-11-11 13:39:18 +0300

    init commit

---
+++ computer-graphics-raytracing/View.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace computer_graphics_raytracing
+{
+    class View
+    {
+    }
+}

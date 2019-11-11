58f3c9d65ee0641f6e52bc3c35ddde9e77e0864f
anton <anton@DESKTOP-6FI73JL>
2019-11-11 14:02:58 +0300

    nichego poka net

--- computer-graphics-raytracing/View.cs
+++ computer-graphics-raytracing/View.cs
@@ -3,10 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using OpenTK;
+using OpenTK.Graphics.OpenGL;
 
 namespace computer_graphics_raytracing
 {
     class View
     {
+        void loadShader(String filename, ShaderType type, int program, out int address)
+        {
+            address = GL.CreateShader(type);
+            using (System.IO.StreamReader sr = new System.IO.StreamReader(filename))
+            {
+                GL.ShaderSource(address, sr.ReadToEnd());
+            }
+            GL.CompileShader(address);
+            GL.AttachShader(program, address);
+            Console.WriteLine(GL.GetShaderInfoLog(address));
+        }
     }
 }
